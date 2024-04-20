794bda9b2d1089b7002ef5b37e11b0611c73795b
noman hussain <shoaibnavi@gmail.com>
2024-04-20 15:49:02 +0530

    branch2 added

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ function newFeatureAdd(){
   console.log('this is new feature')
   console.log('this is new feature A')
 }
+
+function newFeatureAddB(){
+  console.log('this is new feature')
+  console.log('this is new feature B')
+}
